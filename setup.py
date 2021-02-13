40665eedc787afc5d0a00bf4fe68c8e274e9dd17
Hara-Yuma <ysmst35@gmail.com>
2021-02-13 22:23:11 +0900

    fix: setup.pyを修正

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ setup(
     version='0.1',
     description=u'時系列データをより理解しやすい形で記録し、再生するアプリケーションのフレームワーク。',
     author='uMa (Hara-Yuma)',
-    url='https://github.com/Hara-Yuma/TimeSeriesSensing'
+    url='https://github.com/Hara-Yuma/TimeSeriesSensing',
+    packages=['tss']
 )
