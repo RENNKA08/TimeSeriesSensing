37d61976a0f3f145e002915480f32537ce55e3d0
Hara-Yuma <ysmst35@gmail.com>
2021-02-13 17:45:17 +0900

    feature: run.shを更新

--- run.sh
+++ run.sh
@@ -2,4 +2,10 @@
 
 ROOT="$(dirname $(realpath $0))"
 
-python $ROOT/main.py
+if [ $1 -eq "player" ]; then
+    python $ROOT/tss-player.py
+elif [ $1 -eq "recorder" ]; then
+    python $ROOT/tss-recorder.py
+else
+    echo '引数にはplayerかrecorderを指定してください'
+fi
