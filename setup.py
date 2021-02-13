edacd7ceb912ea268003d0a0c0c8aa929817abad
Hara-Yuma <ysmst35@gmail.com>
2021-02-13 19:56:03 +0900

    feature: setup.pyを追加

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='tss',
+    version='0.1',
+    description=u'時系列データをより理解しやすい形で記録し、再生するアプリケーションのフレームワーク。',
+    author='uMa (Hara-Yuma)',
+    url='https://github.com/Hara-Yuma/TimeSeriesSensing'
+)
