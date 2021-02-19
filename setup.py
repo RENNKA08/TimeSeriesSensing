dc548817c72d668475ba76f3162f30c916af7a17
Hara-Yuma <ysmst35@gmail.com>
2021-02-19 17:55:34 +0900

    feature: setup.pyに依存パッケージの指定を追加

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ from setuptools import setup
 setup(
     name='tss',
     version='0.1',
+    install_requires=[
+        'opencv_contrib_python',
+        'Pillow',
+        'pyserial'
+    ],
     description=u'時系列データをより理解しやすい形で記録し、再生するアプリケーションのフレームワーク。',
     author='uMa (Hara-Yuma)',
     url='https://github.com/Hara-Yuma/TimeSeriesSensing',
